440cb09a26868c614b289c06b4e76047648ee2b4
imReset <fffffreinds101@gmail.com>
2022-11-30 20:43:37 -0800

    add button and countdown

--- src/components/Countdown.js
+++ src/components/Countdown.js
@@ -1,7 +1,28 @@
-import React from "react";
+import { useContext } from "react";
+import { CountdownCircleTimer } from "react-countdown-circle-timer";
+import { Settings } from "./Settings";
 
-const Countdown = () => {
-  return <div>Countdown</div>;
+const Countdown = ({ key, timer, animate, remainder }) => {
+  return (
+    <CountdownCircleTimer
+      key={key}
+      isPlaying={animate}
+      duration={timer * 60}
+      colors={[
+        ["e0b371", 0.33],
+        ["e0b371", 0.33],
+        ["e0b371", 0.33],
+      ]}
+      strokeWidth={6}
+      size={20}
+      trailColor="#800000"
+      onComplete={() => {
+        stopCountdown();
+      }}
+    >
+      {remainder}
+    </CountdownCircleTimer>
+  );
 };
 
 export default Countdown;
--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import Settings from "./components/Settings";
+import SettingsProvider from "./components/Settings";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
+ReactDOM.render(
+  <SettingsProvider>
     <App />
-  </React.StrictMode>
+  </SettingsProvider>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
